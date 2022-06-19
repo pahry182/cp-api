6dcb328036914dbb205db4d8e41c6355cc150856
Muhammad Rauf <pahry182@gmail.com>
2022-06-19 13:43:31 +0700

    update

--- main.py
+++ main.py
@@ -50,5 +50,5 @@ api.add_resource(AccountManager, "/account/<int:account_id>")
 def index():
     return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
